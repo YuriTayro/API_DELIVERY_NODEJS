90d067200c8223035234200a9eef949d6e2dc481
YuriTayro <yuri.tayrocnp@gmail.com>
2025-03-05 21:44:23 -0300

    mudança no index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express'); // Importa o Swagger UI
-const yaml = require('yamljs'); // Importa o YAMLJS
+const fs = require('fs');
+const path = require('path'); // Necessário para resolver o caminho do arquivo no Vercel
 const { generateToken } = require('./auth');
 const { checkToken, getAddresses, addAddress } = require('./delivery');
 
-// Carregar o arquivo swagger.yaml
-const swaggerDocument = yaml.load('./swagger.yaml'); // Aponte para o caminho correto do seu arquivo YAML
-
 const app = express();
 app.use(bodyParser.json());
+
+// Carregar o arquivo swagger.yaml de forma robusta
+const swaggerDocument = fs.readFileSync(path.join(__dirname, '..', 'swagger.yaml'), 'utf8');
 
 // Roteamento
 app.post('/api/auth/token', generateToken);
@@ -17,7 +18,7 @@ app.get('/api/delivery/addresses', checkToken, getAddresses);
 app.post('/api/delivery/addresses', checkToken, addAddress);
 
 // Rota para a documentação do Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(JSON.parse(swaggerDocument)));
 
 // Inicia o servidor
 app.listen(3000, () => {
