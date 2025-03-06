57c921b93a8c09162b3258ba2e139cc8c0aadf82
YuriTayro <yuri.tayrocnp@gmail.com>
2025-03-06 10:38:52 -0300

    Modificações para acrescentar a doc da API no Swagger UI

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const { generateToken } = require('./auth');
 const { checkToken, getAddresses, addAddress } = require('./delivery');
 
 const app = express();
 app.use(bodyParser.json());
 
+// Carrega o arquivo swagger.yaml
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+// Rota para a documentação Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 // Roteamento
 app.post('/api/auth/token', generateToken);
-
 app.get('/api/delivery/addresses', checkToken, getAddresses);
 app.post('/api/delivery/addresses', checkToken, addAddress);
 
@@ -17,4 +24,4 @@ app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
 
-module.exports = app; // Exporta para deploy no Vercel
+module.exports = app;
