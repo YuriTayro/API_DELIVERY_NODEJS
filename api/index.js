9611334a37143ef0c2bbddfde1cdb83d613d4bc1
YuriTayro <yuri.tayrocnp@gmail.com>
2025-03-05 21:38:02 -0300

    Implantando o swagger.yaml

--- api/index.js
+++ api/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express'); // Importa o Swagger UI
+const yaml = require('yamljs'); // Importa o YAMLJS
 const { generateToken } = require('./auth');
 const { checkToken, getAddresses, addAddress } = require('./delivery');
+
+// Carregar o arquivo swagger.yaml
+const swaggerDocument = yaml.load('./swagger.yaml'); // Aponte para o caminho correto do seu arquivo YAML
 
 const app = express();
 app.use(bodyParser.json());
 
 // Roteamento
 app.post('/api/auth/token', generateToken);
-
 app.get('/api/delivery/addresses', checkToken, getAddresses);
 app.post('/api/delivery/addresses', checkToken, addAddress);
+
+// Rota para a documentação do Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Inicia o servidor
 app.listen(3000, () => {
